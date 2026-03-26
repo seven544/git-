3c6abf2da97d85bccfd82f4a19ca004132679c71
Wu_Na <2322850139@qq.com>
2026-03-26 17:39:31 +0800

    change HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // 打印一次 HelloWorld
+        System.out.println("HelloWorld");
         System.out.println("HelloWorld");
     }
 }

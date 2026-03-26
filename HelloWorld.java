cd8cbe3dadceac22a673c8529b5249a25eb12278
Wu_Na <2322850139@qq.com>
2026-03-26 17:14:37 +0800

    first commit: add README.md

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 打印一次 HelloWorld
+        System.out.println("HelloWorld");
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Git 实验项目
+这是我的第一个 Git 仓库
+包含 HelloWorld.java 示例代码
